64f29359c452563aba5c1bb5864d16fac907a6b1
ValterMonteiro <valtinms@gmail.com>
2023-05-30 22:33:30 -0400

    update

--- src/main/java/br/edu/ifms/aula/turma/TurmaRepository.java
+++ src/main/java/br/edu/ifms/aula/turma/TurmaRepository.java
@@ -1,10 +1,8 @@
 package br.edu.ifms.aula.turma;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
-@Repository
 public interface TurmaRepository extends JpaRepository<Turma, Long> {
 	
 
