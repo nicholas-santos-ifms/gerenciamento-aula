47df766d44f382a8a0c26021766d09b5e146ddd8
ValterMonteiro <valtinms@gmail.com>
2023-05-30 21:52:11 -0400

    create EstudanteRepository

---
+++ src/main/java/br/edu/ifms/aula/estudante/EstudanteRepository.java
@@ -0,0 +1,7 @@
+package br.edu.ifms.aula.estudante;
+
+import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
+
+public class EstudanteRepository extends JpaRepositoryExtension<Estudante, Long>{
+  
+}
