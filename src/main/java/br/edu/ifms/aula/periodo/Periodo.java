5ffa3462b6c740abd5b6b2f805f771166580b85a
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-06-07 20:23:36 -0400

    Salvando Classes antes da Herança

--- src/main/java/br/edu/ifms/aula/periodo/Periodo.java
+++ src/main/java/br/edu/ifms/aula/periodo/Periodo.java
@@ -4,10 +4,15 @@
  */
 package br.edu.ifms.aula.periodo;
 
+import br.edu.ifms.arch.BaseObject;
 import br.edu.ifms.aula.tipo_periodo.TipoPeriodo;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import java.io.Serializable;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
@@ -15,23 +20,38 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-@Entity
+
 @Data
-@EqualsAndHashCode
+@EqualsAndHashCode (callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@SuperBuilder
+@Entity
+@SequenceGenerator(sequenceName = "campus_sequence", name = "baseObjectSequence", allocationSize = 1)
+public class Periodo extends BaseObject{
 
-public class Periodo implements Serializable{
     @Id
     @GeneratedValue
     private Long id;
-    private String nome;
-    private TipoPeriodo tipoPeriodo;
-    private int numero;
-    private int ano;
-    private LocalDate inicio;
-    private LocalDate fim;
     
+    @Column(columnDefinition = "varchar(255) not null")
+    private String nome;
+    
+    @Column(columnDefinition = "varchar(255) not null")
+    @Enumerated(EnumType.STRING)
+    private TipoPeriodo tipoPeriodo;
+    
+    @Column(columnDefinition = "int not null")
+    private Integer numero;
+    
+    @Column(columnDefinition = "int not null")
+    private Integer ano;
+    
+    @Column(columnDefinition = "time with time zone not null")
+    private LocalDate inicio;
+    
+    @Column(columnDefinition = "time with time zone not null")
+    private LocalDate fim;
 }
