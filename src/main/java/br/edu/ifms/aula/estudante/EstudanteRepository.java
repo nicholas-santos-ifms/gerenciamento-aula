68d2a8e062ed405f15f997d439addcd6228e45f4
ValterMonteiro <valtinms@gmail.com>
2023-06-15 20:46:19 -0400

    update EstudanteRepository

--- src/main/java/br/edu/ifms/aula/estudante/EstudanteRepository.java
+++ src/main/java/br/edu/ifms/aula/estudante/EstudanteRepository.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.edu.ifms.aula.estudante;
+package br.edu.ifms.aula.Estudante;
 
 import br.edu.ifms.arch.repository.IArchRepository;
 
