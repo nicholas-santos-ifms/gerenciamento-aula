18297a5afafb63df676584b760f9f5848d03a440
ValterMonteiro <valtinms@gmail.com>
2023-06-15 20:47:11 -0400

    update EstudanteNotFoundException

--- src/main/java/br/edu/ifms/aula/estudante/EstudanteNotFoundException.java
+++ src/main/java/br/edu/ifms/aula/estudante/EstudanteNotFoundException.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.edu.ifms.aula.estudante;
+package br.edu.ifms.aula.Estudante;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
