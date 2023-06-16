f6de0c9edd222c019fe2ddfcfca62a64117bc91d
Nicholas Eduardo Lopes dos Santos <1513003@ifms.edu.br>
2023-06-15 21:03:00 -0400

    criação do horário e da carga horária

---
+++ src/main/java/br/edu/ifms/aula/carga_horaria/CargaHorariaForm.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.aula.carga_horaria;
+
+import br.edu.ifms.arch.dto.AdapterBaseObjectForm;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+/**
+ *
+ * @author 1513003
+ */
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class CargaHorariaForm extends AdapterBaseObjectForm {
+
+    private Integer duracao;
+
+    private MedidaTempo medidaTempo;
+}
---
+++ src/main/java/br/edu/ifms/aula/carga_horaria/CargaHorariaMapper.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.edu.ifms.aula.carga_horaria;
+
+import br.edu.ifms.arch.BaseObjectMapper;
+import br.edu.ifms.arch.ISimpleMapper;
+import java.util.List;
+import org.mapstruct.InheritConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+
+/**
+ *
+ * @author nicho
+ */
+@Mapper(
+        config = BaseObjectMapper.class
+)
+public interface CargaHorariaMapper extends ISimpleMapper<CargaHoraria, CargaHorariaDto, CargaHorariaForm> {
+
+    public static final CargaHorariaMapper INSTANCE = Mappers.getMapper(CargaHorariaMapper.class);
+
+    @InheritConfiguration(name = "toEntity")
+    @Override
+    public CargaHoraria formToEntity(CargaHorariaForm dto);
+
+    @Override
+    public CargaHoraria dtoToEntity(CargaHorariaDto dto);
+
+    @Override
+    public CargaHorariaDto toDto(CargaHoraria entity);
+
+    @InheritConfiguration(name = "update")
+    @Override
+    public CargaHoraria update(CargaHorariaForm dto, @MappingTarget CargaHoraria entity);
+
+    @Override
+    public List<CargaHoraria> toEntityList(List<CargaHorariaDto> items);
+
+    @Override
+    public List<CargaHorariaDto> toDtoList(List<CargaHoraria> items);
+
+    @Override
+    public default Page<CargaHorariaDto> toDtoPage(Page<CargaHoraria> items) {
+        return items.map(entity -> toDto(entity));
+    }
+}
