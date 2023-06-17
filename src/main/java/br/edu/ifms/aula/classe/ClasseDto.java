68ad9e327fc42ef9a8ed14ddaf408c56c6cb2aab
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2023-06-16 21:38:53 -0400

    :recycle: Refatoração e code clean-up

--- src/main/java/br/edu/ifms/aula/classe/ClasseDto.java
+++ src/main/java/br/edu/ifms/aula/classe/ClasseDto.java
@@ -7,16 +7,12 @@ package br.edu.ifms.aula.classe;
 import br.edu.ifms.aula.disciplina.DisciplinaDto;
 import br.edu.ifms.aula.periodo.PeriodoDto;
 import br.edu.ifms.aula.turma.TurmaDto;
+import lombok.*;
+
 import java.time.LocalDate;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 /**
- *
  * @author 1513003
  */
 @Getter
@@ -31,12 +27,12 @@ public class ClasseDto {
     private Integer numeroAulas;
 
     private LocalDate inicio;
-    
+
     private DisciplinaDto disciplina;
-    
+
     private PeriodoDto periodo;
-    
+
     private TurmaDto turma;
-    
+
     private List<HorarioDto> horarios;
 }
--- src/main/java/br/edu/ifms/aula/config/CorsConfiguration.java
+++ src/main/java/br/edu/ifms/aula/config/CorsConfiguration.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 /**
- *
  * @author 1513003
  */
 @Configuration
