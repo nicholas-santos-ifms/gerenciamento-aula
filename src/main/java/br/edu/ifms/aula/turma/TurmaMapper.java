f76eee2c836f9ae3c221f434ab12b4632376475b
ValterMonteiro <valtinms@gmail.com>
2023-06-21 14:43:29 -0400

    update TurmaMapper

--- src/main/java/br/edu/ifms/aula/turma/TurmaMapper.java
+++ src/main/java/br/edu/ifms/aula/turma/TurmaMapper.java
@@ -2,26 +2,29 @@ package br.edu.ifms.aula.turma;
 
 import br.edu.ifms.arch.BaseObjectMapper;
 import br.edu.ifms.arch.ISimpleMapper;
-import br.edu.ifms.aula.curso.CursoMapper;
+import br.edu.ifms.aula.turma.Turma;
+import br.edu.ifms.aula.turma.TurmaForm;
 import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
+
 @Mapper(
-        config = BaseObjectMapper.class,
-        uses = { CursoMapper.class }
+        config = BaseObjectMapper.class
 )
 public interface TurmaMapper extends ISimpleMapper<Turma, TurmaDto, TurmaForm> {
 
+    
+   
     public static final TurmaMapper INSTANCE = Mappers.getMapper(TurmaMapper.class);
-
     @InheritConfiguration(name = "toEntity")
     @Override
     public Turma formToEntity(TurmaForm dto);
-
+    
     @InheritConfiguration(name = "update")
+   
     @Override
     public Turma update(TurmaForm dto, @MappingTarget Turma entity);
 
@@ -29,9 +32,6 @@ public interface TurmaMapper extends ISimpleMapper<Turma, TurmaDto, TurmaForm> {
     @Override
     public Turma dtoToEntity(TurmaDto dto);
 
-    @Mapping(target = "nomeCurso", source = "entity.curso.nome")
-    @Mapping(target = "nomeCampus", source = "entity.curso.campus.nome")
-    @Override
-    public TurmaDto toDto(Turma entity);
-
+    
+   
 }
