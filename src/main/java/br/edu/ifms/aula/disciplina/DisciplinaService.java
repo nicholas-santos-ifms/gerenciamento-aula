88227cb901759712156ae3d104d5b3c2dc758760
ValterMonteiro <valtinms@gmail.com>
2023-06-21 14:03:11 -0400

    update DisciplinaService

--- src/main/java/br/edu/ifms/aula/disciplina/DisciplinaService.java
+++ src/main/java/br/edu/ifms/aula/disciplina/DisciplinaService.java
@@ -4,10 +4,24 @@
  */
 package br.edu.ifms.aula.disciplina;
 
+import br.edu.ifms.arch.service.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author usuario
  */
-public class DisciplinaService {
+@Service
+public class DisciplinaService 
+        extends AbstractService<Disciplina, Long, DisciplinaForm, DisciplinaRepository> {
+
+    @Autowired
+    @Override
+    public void setRepository(DisciplinaRepository repository) {
+        super.repository = repository;
+        super.setMapper(DisciplinaMapper.INSTANCE);
+    }
+    
     
 }
