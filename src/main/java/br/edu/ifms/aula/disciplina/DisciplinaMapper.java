7f5715e135e08ebed35ffebfb7ec301a5d4f6228
ValterMonteiro <valtinms@gmail.com>
2023-06-21 13:58:33 -0400

    update DisciplinaMapper

--- src/main/java/br/edu/ifms/aula/disciplina/DisciplinaMapper.java
+++ src/main/java/br/edu/ifms/aula/disciplina/DisciplinaMapper.java
@@ -4,10 +4,33 @@
  */
 package br.edu.ifms.aula.disciplina;
 
+import br.edu.ifms.arch.BaseObjectMapper;
+import br.edu.ifms.arch.ISimpleMapper;
+import org.mapstruct.InheritConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
 /**
  *
  * @author usuario
  */
-public class DisciplinaMapper {
+@Mapper(
+        config = BaseObjectMapper.class
+)
+public interface DisciplinaMapper extends ISimpleMapper<Disciplina, DisciplinaDto, DisciplinaForm> {
+
+    public static final DisciplinaMapper INSTANCE = Mappers.getMapper(DisciplinaMapper.class);
+
+    @InheritConfiguration(name = "toEntity")
+    @Override
+    public Disciplina formToEntity(DisciplinaForm dto);
+    
+    @InheritConfiguration(name = "update")
+    @Mapping(target = "id", ignore = true)
+    @Override
+    public Disciplina update(DisciplinaForm dto, @MappingTarget Disciplina entity);
+
     
 }
