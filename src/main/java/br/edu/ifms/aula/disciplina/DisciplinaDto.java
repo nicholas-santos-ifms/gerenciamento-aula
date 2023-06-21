f770cc2bbf0d75dbebebfb88743152f0bf4025d4
ValterMonteiro <valtinms@gmail.com>
2023-06-21 13:55:57 -0400

    update DisciplinaDto

--- src/main/java/br/edu/ifms/aula/disciplina/DisciplinaDto.java
+++ src/main/java/br/edu/ifms/aula/disciplina/DisciplinaDto.java
@@ -4,10 +4,25 @@
  */
 package br.edu.ifms.aula.disciplina;
 
+import br.edu.ifms.arch.dto.AdapterBaseObjectDto;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+
 /**
  *
  * @author usuario
  */
-public class DisciplinaDto {
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+public class DisciplinaDto extends AdapterBaseObjectDto {
     
+	 private String cargaHoraria;
+	    private String ementa;
 }
